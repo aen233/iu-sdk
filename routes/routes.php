4cb5a9b7b67b21f56429f7497da028bec220444a
zhanghe <284767378@qq.com>
2019-03-29 16:50:56 +0800

    first commit

---
+++ routes/routes.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qian
+ * Date: 2019/3/29
+ * Time: 16:30
+ */
+
+use Aen233\IUSDK\Http\Handlers\DocHandler;
+use Aen233\IUSDK\Http\Handlers\LogHandler;
+use Illuminate\Support\Facades\Route;
+
+Route::get('{modules}/doc/{name}', ['uses' => DocHandler::class]);
+Route::get('{modules}/log', ['uses' => LogHandler::class]);
--- src/IUSDKServiceProvider.php
+++ src/IUSDKServiceProvider.php
@@ -9,9 +9,11 @@ class IUSDKServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadViewsFrom(__DIR__ . '/../resources/views/iu-sdk', 'iu-sdk'); // 视图目录指定
+        //注册扩展包路由，使用php artisan route:list 命令可以查看是否生效
+        $this->loadRoutesFrom(__DIR__.'/../routes/routes.php');
         $this->publishes([
             __DIR__ . '/../resources/views/iu-sdk' => base_path('resources/views/iu-sdk'),  // 发布视图目录到resources 下
-//            __DIR__ . '/../config/iu-sdk.php'      => config_path('iu-sdk.php'), // 发布配置文件到 laravel 的config 下
+//            __DIR__ . '/../config/routes.php'      => config_path('routes.php'), // 发布配置文件到 laravel 的config 下
         ]);
     }
 }
